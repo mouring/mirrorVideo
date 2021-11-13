b185266603c73575e6e3f559c173fefce6d5c2c1
Mouring <mouring@eviladmin.org>
2021-11-12 22:02:54 -0600

    Random updates, including initial 'search' if ID or channel not found.

--- src/mirrorVideos.py
+++ src/mirrorVideos.py
@@ -24,6 +24,10 @@ class googleAPI:
         self.url = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&" + url + "&key=" + self.apiKey
 
 
+    # q={UserName}
+    def search(self, url):
+        self.url = "https://www.googleapis.com/youtube/v3/search?part=snippet&" + url + "&key=" + self.apiKey
+
     # maxResults={max rez}&playlistId={listId}&pageToken={pageToken}
     def playlistItems(self, url):
         self.url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&" + url + \
@@ -50,6 +54,11 @@ class ytChannelMirror:
     fetchDir  = '/opt/yt/download'
     fetchFile = '%(uploader)s-%(title)s.%(ext)s'
 
+  # fetchFormat = 'bestvideo[ext=mp4]+bestaudio[ext=m4a]'
+    fetchFormat = "bestvideo+bestaudio[ext=m4a]/best"
+    fetchDir = "/opt/yt/download"
+    fetchFile = "%(uploader)s-%(title)s.%(ext)s"
+
     def __init__(self, mapName, apiKey):
         self.mapName = mapName
         self.listId = os.path.splitext(os.path.basename(mapName))[0]
@@ -59,79 +68,98 @@ class ytChannelMirror:
         self.gAPI.channels("forUsername=" + self.listId)
         data = self.gAPI.jsondata()
 
-        if data['pageInfo']['totalResults']:
-            self.listId = data['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+        if "items" in data:
+            self.listId = data["items"][0]["contentDetails"]["relatedPlaylists"][
+                "uploads"
+            ]
 
         else:
             self.gAPI.channels("id=" + self.listId)
             data = self.gAPI.jsondata()
 
-            if data['pageInfo']['totalResults']:
-                self.listId = data['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+            if "items" in data:
+                self.listId = data["items"][0]["contentDetails"]["relatedPlaylists"][
+                    "uploads"
+                ]
+            else:
+                print(data)
+                print("-----")
+                self.gAPI.search("q=" + self.listId +"&type=channel")
+                data = self.gAPI.jsondata()
+                print(json.dumps(data,indent=4, sort_keys=True))
+                exit(1)
 
 
     def ChannelInfo(self):
-        channelTitle = ''
+        channelTitle = ""
 
         for value in self.mapData:
-            if 'channelTitle' in self.mapData[value]:
-                channelTitle = self.mapData[value]['channelTitle']
+            if "channelTitle" in self.mapData[value]:
+                channelTitle = self.mapData[value]["channelTitle"]
 
-        print("Title: ", channelTitle, "(" + self.channelId , "/" , self.listId + ")")
-
+        print("Title: ", channelTitle, "(" + self.channelId, "/", self.listId + ")")
 
     def setSkipDownload(self):
         self.skipDownload = True
 
-
-    def processVideoList(self, data,map,nextToken):
+    def processVideoList(self, data, map, nextToken):
         for value in data:
-            videoId = value['snippet']['resourceId']['videoId']
+            videoId = value["snippet"]["resourceId"]["videoId"]
             if videoId not in map:
                 print("New Entry: " + videoId)
                 map[videoId] = {
-                  "title": value['snippet']['title'],
-                  "publishedAt": value['snippet']['publishedAt'],
-                  "channelTitle": value['snippet']['channelTitle'],
-                  "fetched": self.fetch(videoId) 
-                } 
+                    "title": value["snippet"]["title"],
+                    "publishedAt": value["snippet"]["publishedAt"],
+                    "channelTitle": value["snippet"]["channelTitle"],
+                    "fetched": self.fetch(videoId),
+                }
             else:
-               nextToken = ''
+                nextToken = ""
         return nextToken, map
-
 
     def fetch(self, code):
         if self.skipDownload == True:
             return True
 
-        args = shlex.split('/usr/bin/youtube-dl -f \'' + self.fetchFormat + '\' -o ' + self.fetchDir + '/' + self.fetchFile + ' https://www.youtube.com/watch?v=' + code)
+        args = shlex.split(
+            "/opt/yt/bin/youtube-dlc -f '"
+            + self.fetchFormat
+            + "' -o "
+            + self.fetchDir
+            + "/"
+            + self.fetchFile
+            + " https://www.youtube.com/watch?v="
+            + code
+        )
         print("D/L: ", args)
         p = subprocess.Popen(args)
         d = p.communicate()
 
-        if (p.returncode == 0):
+        if p.returncode == 0:
             return True
 
         print("Failure Return: ", p.returncode)
         return False
 
-    def videosFromListId(self, pageToken = ''):
+    def videosFromListId(self, pageToken=""):
         url = "&playlistId=" + self.listId
         if pageToken:
             url = url + "&pageToken=" + pageToken
 
         self.gAPI.playlistItems(url)
         data = self.gAPI.jsondata()
+        if 'error' in data:
+            print("Error: ",data['error'])
+#            exit(1)
 
-        if 'nextPageToken' in data:
-            return data['nextPageToken'], data['items']
+        if "nextPageToken" in data:
+            return data["nextPageToken"], data["items"]
         else:
-            return '', data['items']
-
+            return "", data["items"]
 
     def loadMap(self):
         if os.path.isfile(self.mapName):
-            with open(self.mapName, 'r') as infile:
+            with open(self.mapName, "r") as infile:
                 map = json.load(infile)
         else:
             map = {}
@@ -139,17 +167,16 @@ class ytChannelMirror:
         self.mapData = map
 
     def saveMap(self):
-        with open(self.mapName, 'w') as outfile:
+        with open(self.mapName, "w") as outfile:
             json.dump(self.mapData, outfile)
-
 
     def process(self):
         if self.error == True:
-           return
+            return
 
         map = self.mapData
 
-        nextToken, list = self.videosFromListId('')
+        nextToken, list = self.videosFromListId("")
         nextToken, map = self.processVideoList(list, map, nextToken)
 
         while nextToken:
@@ -157,50 +184,47 @@ class ytChannelMirror:
             nextToken, map = self.processVideoList(list, map, nextToken)
 
         for value in map:
-            if 'fetched' in map[value]:
-               fetched = map[value]['fetched']
+            if "fetched" in map[value]:
+                fetched = map[value]["fetched"]
             else:
-               fetched = False; 
+                fetched = False
 
             if not fetched:
-                map[value]['fetched'] = self.fetch(value)
+                map[value]["fetched"] = self.fetch(value)
 
         self.mapData = map
 
-
     def displayMap(self):
         if self.error == True:
-           return
+            return
 
         for value in self.mapData:
-            if self.mapData[value]['fetched'] == True:
-                print("[X] ", end = '')
+            if self.mapData[value]["fetched"] == True:
+                print("[X] ", end="")
             else:
-                print("[ ] ", end = '')
-            print(value, end = ' ')
-            print(self.mapData[value]['publishedAt'], end = ' ')
-            print(self.mapData[value]['title'])
-
+                print("[ ] ", end="")
+            print(value, end=" ")
+            print(self.mapData[value]["publishedAt"], end=" ")
+            print(self.mapData[value]["title"])
 
     def toggleDownloadStatus(self, valueDownload):
-         if  valueDownload in self.mapData:
-             if self.mapData[valueDownload]['fetched'] == True:
-                 self.mapData[valueDownload]['fetched'] = False
-                 display = "Not Downloaded"
-             else:
-                 self.mapData[valueDownload]['fetched'] = True
-                 display = "Downloaded"
+        if valueDownload in self.mapData:
+            if self.mapData[valueDownload]["fetched"] == True:
+                self.mapData[valueDownload]["fetched"] = False
+                display = "Not Downloaded"
+            else:
+                self.mapData[valueDownload]["fetched"] = True
+                display = "Downloaded"
 
-             print("Set to", display, "-", self.mapData[valueDownload]['title'])
-         else:
-             print("Key not found in map.")
-
+            print("Set to", display, "-", self.mapData[valueDownload]["title"])
+        else:
+            print("Key not found in map.")
 
 
 def main(argv):
-    mapName = ''
-    valueDownload = ''
-    apiKey=""
+    mapName = ""
+    valueDownload = ""
+    apiKey = ""
 
     SHOW_HELP = False
     DISPLAY_MAP = False
@@ -211,33 +235,33 @@ def main(argv):
     except getopt.GetoptError:
         SHOW_HELP = True
     else:
-      for opt, arg in opts:
-          if opt == '-h':
-              SHOW_HELP = True
-          elif opt == '-m':
-              mapName = arg
-          elif opt == '-s':
-              skipDownload = True
-          elif opt == '-a':
-              apiKey = arg
-          elif opt == '-t':
-              valueDownload = arg
-          elif opt == '-l':
-              DISPLAY_MAP = True
+        for opt, arg in opts:
+            if opt == "-h":
+                SHOW_HELP = True
+            elif opt == "-m":
+                mapName = arg
+            elif opt == "-s":
+                skipDownload = True
+            elif opt == "-a":
+                apiKey = arg
+            elif opt == "-t":
+                valueDownload = arg
+            elif opt == "-l":
+                DISPLAY_MAP = True
     if not mapName:
-       print("Error: No map file defined.")
-       SHOW_HELP = True
-    if apiKey == '':
-       print("Error: No Google API Key defined.")
-       SHOW_HELP = True
+        print("Error: No map file defined.")
+        SHOW_HELP = True
+    if apiKey == "":
+        print("Error: No Google API Key defined.")
+        SHOW_HELP = True
 
     if SHOW_HELP:
-        print('mirrorVideo -a <apikey> -m <mapFile> [options]')
+        print("mirrorVideo -a <apikey> -m <mapFile> [options]")
         print("	-a <apikey>		-- Set Youttube API key [required]")
         print("	-t <value>		-- Toggle download status")
         print("	-l 			-- List vidoes in Map")
         print("	-m <mapFile>		-- Map File for history [required]")
-        print("	-s            		-- Set all items to Download") 
+        print("	-s            		-- Set all items to Download")
         print("	-h            		-- This silly help text")
         sys.exit(2)
 
@@ -246,8 +270,8 @@ def main(argv):
     ytChan.ChannelInfo()
 
     if DISPLAY_MAP == True:
-        ytChan.displayMap() 
-    elif valueDownload != '':
+        ytChan.displayMap()
+    elif valueDownload != "":
         ytChan.toggleDownloadStatus(valueDownload)
         ytChan.saveMap()
     else:
